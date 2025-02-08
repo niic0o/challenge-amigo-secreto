1ee1c8a7b7d2b759ce85efc1bef99079bd154de9
nico <nicosena10@gmail.com>
2025-02-08 16:09:12 -0300

    Muestra la lista de amigos en pantalla

--- app.js
+++ app.js
@@ -1,38 +1,56 @@
-let listaAmigos = []
-
+let listaAmigos = [];
+let listaFront = document.getElementById("listaAmigos");
 //validaciónes
-const inputAmigo = document.getElementById('amigo');
+const inputAmigo = document.getElementById("amigo");
 
-inputAmigo.addEventListener('blur', (e) => {
-    const valor = e.target.value;
-    if (!valor.match(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/)){
-        alert('Solo se permiten letras');
-    };
+inputAmigo.addEventListener("blur", (e) => {
+  const valor = e.target.value;
+  if (!valor.match(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$/)) {
+    alert("Solo se permiten letras");
+  }
 });
 
-function agregarAmigo(){
-    let amigo = inputAmigo.value;
-    console.log(listaAmigos);
-    if (amigo === ''){
-        alert('Por favor, inserte un nombre.');
-    }else{
-        existeAmigo(amigo);        
-        inputAmigo.value = '';
-    };
-};
+function agregarAmigo() {
+  let amigo = inputAmigo.value;
+  if (amigo === "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    existeAmigo(amigo);
+    inputAmigo.value = "";
+    mostrarAmigo();
+  }
+}
 
-function existeAmigo(p_amigo){
-    if (listaAmigos.some((elemento) => elemento.toLowerCase() === p_amigo.toLowerCase())){
-        alert('Este amigo ya existe en la lista');
-        }else{
-            listaAmigos.push(p_amigo);
-        };
-};
-// Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto
-// ingresado por el usuario.
+function existeAmigo(p_amigo) {
+  if (
+    listaAmigos.some(
+      (elemento) => elemento.toLowerCase() === p_amigo.toLowerCase()
+    )
+  ) {
+    alert("Este amigo ya existe en la lista");
+  } else {
+    listaAmigos.push(p_amigo);
+  }
+}
 
-// Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío.
-// Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+function mostrarAmigo() {
+  listaFront.innerHTML = "";
+  listaAmigos.forEach((amigo) => {
+    let li = document.createElement("li");
+    li.textContent = amigo;
+    listaFront.appendChild(li); //asigno li a su padre ul
+  });
+}
 
-// Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push()
-// Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
+// Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML.
+// Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+// Tareas específicas:
+// Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector()
+// para seleccionar la lista donde se mostrarán los amigos.
+
+// Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+
+// Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>)
+// para cada título.
+
+// Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
